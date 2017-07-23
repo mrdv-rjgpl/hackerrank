b332537da2e6c409ddd297451e819e519ee605d5
Mardava Gubbi <mrdv.rjgpl@gmail.com>
2017-07-23 07:41:22 +0530

    Modify divisible sum pairs to fix brute forcing.

--- DivisibleSumPairs/divisible_sum_pairs.cpp
+++ DivisibleSumPairs/divisible_sum_pairs.cpp
@@ -6,6 +6,10 @@ using namespace std;
  *
  * \brief Return the number of divisible sum pairs in the vector.
  *
+ * Store the counts of remainder values in a vector.
+ * Take the corresponding counts of 'i' and 'k - i' and multiply them together.
+ * Subtract the repeats in the case of 'i = 0'.
+ *
  * \param[in] n The size of the input vector.
  * \param[in] k The divisor of the sum of potential pairs.
  * \param[in] input The input vector.
@@ -16,6 +20,42 @@ using namespace std;
  *
  */
 int DivisibleSumPairs(int n, int k, vector<int> input)
+{
+	int num_div_sum_pairs;
+	int i;
+	vector<int> remainder_counts(k);
+
+	for(i = 0; i < n; ++i)
+	{
+		++remainder_counts[input[i] % k];
+	}
+
+	num_div_sum_pairs = (remainder_counts[0] * (remainder_counts[0] - 1)) / 2;
+
+	for(i = 1; 2 * i < k; ++i)
+	{
+		num_div_sum_pairs += (remainder_counts[i] * remainder_counts[k - i]);
+	}
+
+	return num_div_sum_pairs;
+}
+
+/**
+ *
+ * \brief Return the number of divisible sum pairs in the vector.
+ *
+ * Use the brute force method - summing each pair and dividing by 'k'.
+ *
+ * \param[in] n The size of the input vector.
+ * \param[in] k The divisor of the sum of potential pairs.
+ * \param[in] input The input vector.
+ *
+ * \return num_div_sum_pairs The number of divisible sum pairs.
+ *
+ * \author Mardava Gubbi <mrdv.rjgpl@gmail.com>
+ *
+ */
+int DivisibleSumPairsBruteForce(int n, int k, vector<int> input)
 {
 	int num_div_sum_pairs = 0;
 	int i;
